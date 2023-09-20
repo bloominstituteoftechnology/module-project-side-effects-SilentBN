06aa8dce60ecdb2dc46010ede2bab981708fd515
SilentBN <61912362+SilentBN@users.noreply.github.com>
2023-09-20 16:11:50 -0400

    Finished

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Comment from './Comment';
+import React from "react";
+import Comment from "./Comment";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
-    <div className='comments-wrapper'>
+    <div className="comments-wrapper">
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((cmt) => (
+        <Comment key={cmt.id} comment={cmt} />
+      ))}
+      */}
     </div>
   );
 };
